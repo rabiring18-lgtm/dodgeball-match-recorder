420b3ce69cfb2831fd1f3e97fd0489d04103a76e
rabiring18-lgtm <rabiring18@gmail.com>
2026-06-21 01:07:59 +0900

    GitHub Pagesの公開設定を追加

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const isGitHubPages = mode === 'github-pages'
+
+  return {
+    base: isGitHubPages ? '/dodgeball-match-recorder/' : '/',
+    plugins: [react()],
+  }
 })
